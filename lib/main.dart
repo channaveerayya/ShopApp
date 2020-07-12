d2849993c8c413e72d4a055eeff72e66bc2b7aec
channaveerayya <channucs0009@gmail.com>
2020-07-12 20:47:21 +0530

    state management, provider,and Listener added

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
-import 'package:ShopApp/screens/product_detail_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import './screens/product_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-          primarySwatch: Colors.blueGrey,
-          accentColor: Colors.lightBlue,
-          fontFamily: 'Lato'),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.productDetailScreenRoute: (ctx) =>
-            ProductDetailScreen(),
-      },
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+            primarySwatch: Colors.blueGrey,
+            accentColor: Colors.lightBlue,
+            fontFamily: 'Lato'),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.productDetailScreenRoute: (ctx) =>
+              ProductDetailScreen(),
+        },
+      ),
     );
   }
 }
