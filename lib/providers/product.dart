6beb0ecd526aa56850c6964361c5f6bc3129a7ab
channaveerayya <channucs0009@gmail.com>
2020-07-21 21:24:37 +0530

    favorite status change using patch

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,23 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavorite() {
+  void toggleFavorite() async {
+    final oldStatus = isFavorite;
+    final url = 'https://fluttertest-5f138.firebaseio.com/products/$id.json';
     isFavorite = !isFavorite;
     notifyListeners();
+    try {
+      final res = await http.patch(url,
+          body: jsonEncode({
+            'isFavorite': isFavorite,
+          }));
+      if (res.statusCode > 400) {
+        isFavorite = oldStatus;
+        notifyListeners();
+      }
+    } catch (e) {
+      isFavorite = oldStatus;
+      notifyListeners();
+    }
   }
 }
