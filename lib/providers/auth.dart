61f552ce6e574af75b178b21f74bdb7984d07c7d
channaveerayya <channucs0009@gmail.com>
2020-07-28 18:37:18 +0530

    allowing to user log in

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,9 +8,10 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signUp(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCfWMEbMiRR-x-y39vPgHHoJ_gXTn8zslU';
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCfWMEbMiRR-x-y39vPgHHoJ_gXTn8zslU";
     final res = await http.post(
       url,
       body: jsonEncode({
@@ -20,5 +21,13 @@ class Auth with ChangeNotifier {
       }),
     );
     print(res.body.toString());
+  }
+
+  Future<void> signUp(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> signIn(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
